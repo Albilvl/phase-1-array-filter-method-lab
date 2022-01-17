8919b491e08957104336fe8e5f22248c5b897127
Al Bilal <bil.bank@yahoo.com>
2022-01-17 13:40:14 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+
+
+
+function findMatching (drivers, driver){
+    return drivers.filter((match) => match.toLowerCase ()=== driver.toLowerCase() )
+}
+
+
+function fuzzyMatch(drivers, testString) {
+    return drivers.filter(
+      (match) =>
+        match.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+  }
+
+  function matchName(drivers, teststring){
+      return drivers.filter((match)=>match.name === teststring)
+  }
